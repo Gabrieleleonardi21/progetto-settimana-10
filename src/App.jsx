9db30bf4a4ef433a71f4716aa67af33b23115efa
Gabriele <leonardi.gabriele9821@gmail.com>
2026-07-10 10:38:43 +0200

    test API con component meteo con button di ricerca

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import './App.css'
+import "./App.css";
+import MeteoComponent from "./assets/componets/MeteoComponet";
 
 function App() {
+  return <MeteoComponent></MeteoComponent>;
 }
 
-export default App
+export default App;
